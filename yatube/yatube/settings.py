b2da94dcf4d985ff29fbe7f8393ed5406efc9146
Andrey Klevtsov <andrewklevt@gmail.com>
2023-08-22 10:34:53 +0300

    README,env,settings

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,19 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=mfuk=h#h&lixr52n5l1p*@gd8lixi@!-_^pn5)7$3$4n=+@f-'
+SECRET_KEY = os.getenv('SECRET_KEY', 'None')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', '') == 'True'
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '[::1]',
-    'testserver',
-    'www.klevtsov.pythonanywhere.com',
-    'klevtsov.pythonanywhere.com',
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
 # Application definition
 
